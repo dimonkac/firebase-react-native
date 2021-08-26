a35fe976e50b8f1684521a416b5fb985beb77d00
ivanrndev <luciku487@gmail.com>
2021-08-26 14:24:42 +0300

    add demo screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const Section: React.FC<{
   title: string;
@@ -20,12 +16,9 @@ const Section: React.FC<{
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const DemoScreen: React.FC<{}> = () => {
   return (
     <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <Section title="Step One">
@@ -40,6 +33,14 @@ const App = () => {
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Screen name={'DemoScreen'} component={DemoScreen} />
+    </NavigationContainer>
   );
 };
 
